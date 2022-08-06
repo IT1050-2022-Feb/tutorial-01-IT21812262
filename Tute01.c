08624dcbb65df3f8f3271d9438d0ce06647f33a6
IT21812262 <110651176+IT21812262@users.noreply.github.com>
2022-08-06 16:16:58 +0000

    Tutorial 1 , Exercise 1 answer

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,21 @@
 #include <stdio.h>
 
 int main() {
+
+  int sub1;
+  int sub2;
+  float sum,avg;
+
+  printf("Input mark of subject 1 : ");
+  scanf("%d",&sub1);
+
+  printf("Input mark of subject 2 : ");
+  scanf("%d",&sub2);
+
+  sum = sub1 + sub2;
+  avg = sum/2;
+
+  printf("Average is :%.2f ", avg);
   
   return 0;
 }
